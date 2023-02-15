36b818850ce75094dfd369ca19d9775d314f57f4
2022hop2c_chinbo <chinbo@iMac-22s-iMac.local>
2023-02-15 17:30:48 +0800

    news

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./page/home";
-
-function RoutesApp() {
+import { Home } from "./page/Home";
+import { News } from "./page/News";
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/News" element={<News />}></Route>
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
-export default RoutesApp;
+export default App;
