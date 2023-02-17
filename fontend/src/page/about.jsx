8edba22630058a59717df119da75b32dd63c0ede
22HOP2C-Azjargal <azuka@Themuulns.local>
2023-02-17 18:05:03 +0800

    about

--- fontend/src/page/about.jsx
+++ fontend/src/page/about.jsx
@@ -25,7 +25,10 @@ export const About = () => {
             className="d-flex flex-column justify-content-center align-items-center mb-3"
           ></div>
         </div>
-        <div className="d-flex flex-row justify-content-center align-items-center">
+        <div
+          className="d-flex flex-row justify-content-center align-items-center"
+          style={{ borderRadius: "5px" }}
+        >
           <img
             style={{ width: "33vw" }}
             className="d-flex flex-row"
@@ -44,7 +47,11 @@ export const About = () => {
             </div>
             <div className="d-flex flex-column">
               <div
-                style={{ fontWeight: "300", color: "white" }}
+                style={{
+                  fontWeight: "300",
+                  color: "white",
+                  borderRadius: "5px",
+                }}
                 className="d-flex ms-5 me-5"
               >
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -95,10 +102,339 @@ export const About = () => {
             className="d-flex flex-column justify-content-center align-items-center mb-3"
           ></div>
         </div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="d-flex flex-row justify-content-center align-items-center">
+          <div
+            style={{ width: "18vw", height: "15vw", borderRadius: "5px" }}
+            className="d-flex flex-column bg-dark ps-4 justify-content-center align-items-start"
+          >
+            <img
+              style={{ width: "50px" }}
+              className="d-flex flex-row mb-3"
+              src="edit.png"
+              alt=""
+            ></img>
+            <h5 style={{ color: "white" }}>Web design</h5>
+            <div
+              style={{
+                width: "16vw",
+                color: "white",
+                fontWeight: "400",
+                fontSize: "16px",
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing, elit. Enim
+              minima, voluptatum ipsam iusto deserunt dignissimos accusamus ea
+              iste odit exercitationem eveniet neque explicabo reiciendis id!
+              Minima dolores corporis voluptatibus, voluptatum.
+            </div>
+          </div>
+          <div
+            style={{ width: "18vw", height: "15vw", borderRadius: "5px" }}
+            className="d-flex flex-column bg-dark ps-4 justify-content-center align-items-start ms-5"
+          >
+            <img
+              style={{ width: "50px" }}
+              className="d-flex flex-row mb-3"
+              src="tv.png"
+              alt=""
+            ></img>
+            <h5 style={{ color: "white" }}>Media & content</h5>
+            <div
+              style={{
+                width: "16vw",
+                color: "white",
+                fontWeight: "400",
+                fontSize: "16px",
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing, elit. Enim
+              minima, voluptatum ipsam iusto deserunt dignissimos accusamus ea
+              iste odit exercitationem eveniet neque explicabo reiciendis id!
+              Minima dolores corporis voluptatibus, voluptatum.
+            </div>
+          </div>
+          <div
+            style={{ width: "18vw", height: "15vw", borderRadius: "5px" }}
+            className="d-flex flex-column bg-dark ps-4 justify-content-center align-items-start ms-5"
+          >
+            <img
+              style={{ width: "50px" }}
+              className="d-flex flex-row mb-3"
+              src="camera.png"
+              alt=""
+            ></img>
+            <h5 style={{ color: "white" }}>Vedeo prod.</h5>
+            <div
+              style={{
+                width: "16vw",
+                color: "white",
+                fontWeight: "400",
+                fontSize: "16px",
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing, elit. Enim
+              minima, voluptatum ipsam iusto deserunt dignissimos accusamus ea
+              iste odit exercitationem eveniet neque explicabo reiciendis id!
+              Minima dolores corporis voluptatibus, voluptatum.
+            </div>
+          </div>
+        </div>
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <div className="d-flex flex-column align-items-center justify-content-center">
+            <div
+              style={{
+                color: "white",
+                width: "57vw",
+                fontWeight: "600",
+                justifyContent: "start",
+              }}
+              className="d-flex text-uppercase mb-2 mt-3"
+            >
+              Our team
+            </div>
+            <div
+              style={{ backgroundColor: "red", height: "3px", width: "58vw" }}
+              className="d-flex flex-column justify-content-center align-items-center mb-3"
+            ></div>
+          </div>
+          <div className="d-flex flex-row align-items-center justify-content-center">
+            <div
+              style={{ width: "310px", height: "340px", borderRadius: "5px" }}
+              className="d-flex bg-dark flex-column justify-content-start align-items-start"
+            >
+              <img
+                className="d-flex justify-content-start align-items-start"
+                style={{ width: "310px", height: "220px" }}
+                src="https://chtstudio.com/demo/news/images/team/1.jpg"
+                alt=""
+              />
+              <h5 style={{ color: "white" }} className="ps-4 pt-3">
+                Azykka
+              </h5>
+              <h6 style={{ color: "white" }} className="ps-4 pt-1">
+                hahahahhahahhahahha
+              </h6>
+              <div className="d-flex flex-row ps-3">
+                <a href="facebook.com">
+                  <img
+                    style={{ width: "24px", height: "16px" }}
+                    className="ps-2"
+                    src="fc.png"
+                    alt=""
+                  />
+                </a>
+                <a href="twitter.com">
+                  <img
+                    style={{ width: "24px", height: "18px" }}
+                    className="ps-2"
+                    src="twitter.png"
+                    alt=""
+                  />
+                </a>
+                <a href="google.com">
+                  <img
+                    style={{ width: "24px", height: "24px" }}
+                    className="ps-2"
+                    src="google.png"
+                    alt=""
+                  />
+                </a>
+                <a href="instagram.com">
+                  <img
+                    style={{ width: "24px", height: "16px" }}
+                    className="ps-2"
+                    src="in.png"
+                    alt=""
+                  />
+                </a>
+                <a href="youtube.com">
+                  <img
+                    style={{ width: "24px", height: "18px" }}
+                    className="ps-2 mt-2"
+                    src="youtube.png"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+            <div
+              style={{ width: "310px", height: "340px", borderRadius: "5px" }}
+              className="d-flex bg-dark flex-column justify-content-start align-items-start ms-3"
+            >
+              <img
+                className="d-flex justify-content-start align-items-start"
+                style={{ width: "310px", height: "220px" }}
+                src="https://chtstudio.com/demo/news/images/team/2.jpg"
+                alt=""
+              />
+              <h5 style={{ color: "white" }} className="ps-4 pt-3">
+                Chinbo
+              </h5>
+              <h6 style={{ color: "white" }} className="ps-4 pt-1">
+                hahahahhahahhahahha
+              </h6>
+              <div className="d-flex flex-row ps-3">
+                <a href="facebook.com">
+                  <img
+                    style={{ width: "24px", height: "16px" }}
+                    className="ps-2"
+                    src="fc.png"
+                    alt=""
+                  />
+                </a>
+                <a href="twitter.com">
+                  <img
+                    style={{ width: "24px", height: "18px" }}
+                    className="ps-2"
+                    src="twitter.png"
+                    alt=""
+                  />
+                </a>
+                <a href="google.com">
+                  <img
+                    style={{ width: "24px", height: "24px" }}
+                    className="ps-2"
+                    src="google.png"
+                    alt=""
+                  />
+                </a>
+                <a href="instagram.com">
+                  <img
+                    style={{ width: "24px", height: "16px" }}
+                    className="ps-2"
+                    src="in.png"
+                    alt=""
+                  />
+                </a>
+                <a href="youtube.com">
+                  <img
+                    style={{ width: "24px", height: "18px" }}
+                    className="ps-2 mt-2"
+                    src="youtube.png"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+            <div
+              style={{ width: "310px", height: "340px", borderRadius: "5px" }}
+              className="d-flex bg-dark flex-column justify-content-start align-items-start ms-3"
+            >
+              <img
+                className="d-flex justify-content-start align-items-start"
+                style={{ width: "310px", height: "220px" }}
+                src="https://chtstudio.com/demo/news/images/team/3.jpg"
+                alt=""
+              />
+              <h5 style={{ color: "white" }} className="ps-4 pt-3">
+                Sumber
+              </h5>
+              <h6 style={{ color: "white" }} className="ps-4 pt-1">
+                hahahahhahahhahahha
+              </h6>
+              <div className="d-flex flex-row ps-3">
+                <a href="facebook.com">
+                  <img
+                    style={{ width: "24px", height: "16px" }}
+                    className="ps-2"
+                    src="fc.png"
+                    alt=""
+                  />
+                </a>
+                <a href="twitter.com">
+                  <img
+                    style={{ width: "24px", height: "18px" }}
+                    className="ps-2"
+                    src="twitter.png"
+                    alt=""
+                  />
+                </a>
+                <a href="google.com">
+                  <img
+                    style={{ width: "24px", height: "24px" }}
+                    className="ps-2"
+                    src="google.png"
+                    alt=""
+                  />
+                </a>
+                <a href="instagram.com">
+                  <img
+                    style={{ width: "24px", height: "16px" }}
+                    className="ps-2"
+                    src="in.png"
+                    alt=""
+                  />
+                </a>
+                <a href="youtube.com">
+                  <img
+                    style={{ width: "24px", height: "18px" }}
+                    className="ps-2 mt-2"
+                    src="youtube.png"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+            <div
+              style={{ width: "310px", height: "340px", borderRadius: "5px" }}
+              className="d-flex bg-dark flex-column justify-content-start align-items-start ms-3"
+            >
+              <img
+                className="d-flex justify-content-start align-items-start"
+                style={{ width: "310px", height: "220px" }}
+                src="https://chtstudio.com/demo/news/images/team/4.jpg"
+                alt=""
+              />
+              <h5 style={{ color: "white" }} className="ps-4 pt-3">
+                Burte
+              </h5>
+              <h6 style={{ color: "white" }} className="ps-4 pt-1">
+                hahahahhahahhahahha
+              </h6>
+              <div className="d-flex flex-row ps-3">
+                <a href="facebook.com">
+                  <img
+                    style={{ width: "24px", height: "16px" }}
+                    className="ps-2"
+                    src="fc.png"
+                    alt=""
+                  />
+                </a>
+                <a href="twitter.com">
+                  <img
+                    style={{ width: "24px", height: "18px" }}
+                    className="ps-2"
+                    src="twitter.png"
+                    alt=""
+                  />
+                </a>
+                <a href="google.com">
+                  <img
+                    style={{ width: "24px", height: "24px" }}
+                    className="ps-2"
+                    src="google.png"
+                    alt=""
+                  />
+                </a>
+                <a href="instagram.com">
+                  <img
+                    style={{ width: "24px", height: "16px" }}
+                    className="ps-2"
+                    src="in.png"
+                    alt=""
+                  />
+                </a>
+                <a href="youtube.com">
+                  <img
+                    style={{ width: "24px", height: "18px" }}
+                    className="ps-2 mt-2"
+                    src="youtube.png"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
